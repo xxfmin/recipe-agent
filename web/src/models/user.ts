27e8834b3b3f496a41a7374180e386584905d451
Felipe Min <felipemin03@gmail.com>
2025-07-03 14:14:47 -0400

    create schema for users

---
+++ web/src/models/user.ts
@@ -0,0 +1,25 @@
+import mongoose, { Schema, Model, Document } from "mongoose";
+
+export interface IUser extends Document {
+  email: string;
+  password: string;
+}
+
+const UserSchema = new Schema<IUser>(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+const User: Model<IUser> =
+  mongoose.models.User || mongoose.model<IUser>("User", UserSchema);
+
+export default User;
